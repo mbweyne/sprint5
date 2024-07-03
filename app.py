ed7f190ebeb2f47d24e4515d543021eddd1b8f64
Marcia Weyne <mbweyne@gmail.com>
2024-07-03 18:26:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-
+import pandas numpy matplotlib seaborn scipy scikit-learn statsmodels notebook plotly beautifulsoup4 requests
 st.header('Jogando uma moeda')
-
 st.write('Ainda não é um aplicativo funcional. Em construção.')
